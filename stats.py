73813a912a034cf736a1caf13c0ddccdc663be9b
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-12-06 18:21:00 -0500

    More troubleshooting

--- stats.py
+++ stats.py
@@ -1,14 +1,13 @@
-import time
 import datetime
 
 
 def record_game_score(maincollection, goldlivesleveljson):
 	print "IN RECORD_GAME_SCORE"
 	print goldlivesleveljson
-	ts = time.time()
-	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-	data = goldlivesleveljson
-	data["time"]=st
-	data["type"]="gamescore"
+	timestamp = '{:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now())
+	print timestamp 
+	dat = goldlivesleveljson
+	dat["time"]=timestamp
+	dat["type"]="gamescore"
 	maincollection.insert(data)
 
