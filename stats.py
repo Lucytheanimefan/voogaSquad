7fcf7564f50cb1dffe5c1190cc2e0ea9fef48a45
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-12-10 20:47:29 -0500

    Change to store data for different games via time stamp

--- stats.py
+++ stats.py
@@ -3,19 +3,32 @@ import JSONEncoder
 from JSONEncoder import *
 
 
+def todaysdate():
+	timestamp = '{:%Y-%b-%d}'.format(datetime.datetime.now())
+	return timestamp
+
 def record_game_score(maincollection, goldlivesleveljson):
 	print "IN RECORD_GAME_SCORE"
 	print goldlivesleveljson
 	timestamp = '{:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now())
 	print timestamp 
 	dat = goldlivesleveljson
-	dat["time"]=[timestamp]
+	dat["time"]=todaysdate()
 	dat["type"]="gamescore"
 	dat["gold"] = [[dat["gold"], timestamp]]
 	dat["level"] =dat["level"][0]
 	dat["lives"] = [[dat["lives"][0], timestamp]]
 	maincollection.insert(dat)
+	if (maincollection.find({"type": "timerecord"}).count() > 0):
+		timerecord = {}
+		timerecord["type"]="timerecord"
+		timerecord["time"]=[todaysdate()]
+		maincollection.insert(timerecord)
+	else:
+		maincollection.update({'type':"timerecord"}, 
+		{'$push': {"time": todaysdate()}})
 
+	
 
 def update_score(maincollection, level, updated_field, num):
 	print "-----------------------------------------------"
@@ -27,7 +40,7 @@ def update_score(maincollection, level, updated_field, num):
 	print num
 	timestamp = '{:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now())
 	print maincollection
-	maincollection.update({'level':level[0]}, 
+	maincollection.update({'time':todaysdate()}, 
 		{'$push': {updated_field: [num,timestamp]}})
 		
 
